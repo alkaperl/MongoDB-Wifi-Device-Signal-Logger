198bd6c371466783e12f9df1abda2323b6fbba9a
Eric Zhao <eric@whimmly.com>
2016-07-16 02:11:47 -0700

    Chances

--- airodumpToDB.js
+++ airodumpToDB.js
@@ -11,19 +11,14 @@ exports.transfer = function(cb){
 	const initialUploadToDB = require('child_process').exec;
 	initialUploadToDB('java -jar ProcessAirodumpOutput.jar -i dump-01.csv', 
 	function (error, stdout, stderr) {
-  	// We are assuming a single signal of 'complete'	
-  	if (stdout=="complete"){
-  		cb();
-  		// Call is complete
-  	} else {
-  		// Maybe attempt to restart upload process
-  		console.log("No proper stdout returned");
-  	}
-	  console.log(`Airodump stdout: ${stdout}`);
-	  console.log(`Airodump stderr: ${stderr}`);
+  	// We are assuming no errors	
 	  if (error) {
 	    console.error(`Airodump exec error: ${error}`);
 	    return;
     }
+  	cb();
+    // Call is complete
+	  console.log(`Airodump stdout: ${stdout}`);
+	  console.log(`Airodump stderr: ${stderr}`);
   });
 }
