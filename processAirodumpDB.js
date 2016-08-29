bd11dab97126ec5a1dbaa9d312b490bdeb04540d
Eric Zhao <eric@whimmly.com>
2016-08-29 02:24:43 -0700

    Changes

--- processAirodumpDB.js
+++ processAirodumpDB.js
@@ -31,54 +31,7 @@ exports.update = function(monConn, cb){
         deviceModel.checkForDevice(monConn, dumpTimeSlice.macAddress, function(locatedDevice){
           callback(null, dumpTimeSlice, locatedDevice[0]);
         });
-      },
-      function(dumpTimeSlice, locatedDevice, callback) {
-        // Create or update device
-        if (locatedDevice){
-          // If there is an exiting device, add airodumpRecord to it
-          deviceModel.addTimeSlice(locatedDevice, dumpTimeSlice._id, function(locatedDevice){
-            // Add probed essids
-            deviceModel.addEssid(locatedDevice, dumpTimeSlice.probedEssids, function(locatedDevice){
-              // Save updated model
-              deviceModels.update({
-                "macAddress": dumpTimeSlice.macAddress
-              }, locatedDevice, true);
-              cb();
-            });
-          });
-        } else {
-          console.log("No device located");
-          // Generate new device if it doesn't exist
-          // Generate new time slice array
-          var newTimeSlices = [];
-          newTimeSlices.push(dumpTimeSlice);
-          // Meanwhile, no need to do same for affiliatedNetworks, 
-          // probedEssids is already array
-          // Build new device
-          var newDevice = new deviceModel({
-            macAddress: dumpTimeSlice.macAddress,
-            timeSlices: newTimeSlices,
-            affiliatedNetworks: dumpTimeSlice.ProbedEssids
-          }); 
-          // Save new device
-          newDevice.save((err) => {  
-            if (err){
-              if (err.code == 11000){
-                console.log("Duplicate device created");
-              }
-            } else {
-              console.log("New device saved");
-              // Assuming new device correctly saves
-              if (index === dumpRecordsCollected.length -1){
-                console.log("DB processing complete");
-                cb();
-              }
-            }
-          });
-        }
-        // Delete the airodumpRecord file
-        airodumpRecord.remove(dumpTimeSlice);
-      }
+      }, handleRecord
     ]); 
   });
 };
@@ -91,3 +44,58 @@ exports.counter = function(monConn, cb){
   }, 20000);
   return timeout;
 };
+
+function handleRecord(dumpTimeSlice, locatedDevice, callback) {
+  // Create or update device
+  if (locatedDevice){
+    // If there is an exiting device, add airodumpRecord to it
+    deviceModel.addTimeSlice(locatedDevice, dumpTimeSlice._id, function(locatedDevice){
+      // Add probed essids
+      deviceModel.addEssid(locatedDevice, dumpTimeSlice.probedEssids, function(locatedDevice){
+        // Save updated model
+        deviceModels.update({
+          "macAddress": dumpTimeSlice.macAddress
+        }, locatedDevice, true);
+        callback();
+      });
+    });
+  } else {
+    saveNewDevice(dumpTimeSlice, dumpRecordsCollected, function(err){
+      if (index === dumpRecordsCollected.length -1){
+        console.log("DB processing complete");
+        waterfallCB();
+      } else {
+        callback(err);
+      }
+    })
+  }
+  // Delete the airodumpRecord file
+  airodumpRecord.remove(dumpTimeSlice);
+}
+
+function saveNewDevice(dumpTimeSlice, dumpRecordsCollected, cb)
+  console.log("No device located");
+  // Generate new device if it doesn't exist
+  // Generate new time slice array
+  var newTimeSlices = [];
+  newTimeSlices.push(dumpTimeSlice);
+  // Meanwhile, no need to do same for affiliatedNetworks, 
+  // probedEssids is already array
+  // Build new device
+  var newDevice = new deviceModel({
+    macAddress: dumpTimeSlice.macAddress,
+    timeSlices: newTimeSlices,
+    affiliatedNetworks: dumpTimeSlice.ProbedEssids
+  }); 
+  // Save new device
+  newDevice.save((err) => {  
+    if (err){
+      if (err.code == 11000){
+        console.log("Duplicate device created");
+      }
+    } else {
+      console.log("New device saved");
+    }
+    cb(err);
+  });
+}
